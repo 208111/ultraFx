f04dd8966f10307c9dbea346be3c6688d1e2b533
donnaken15 <wesley@gmx.it>
2021-02-10 18:32:05 -0500

    ..

--- uFxSys/gaming.cpp
+++ uFxSys/gaming.cpp
@@ -2,7 +2,8 @@
 #include "E:\D3D9\d3d9.h"
 #include "E:\D3D9\d3dx9.h"
 #include <Windows.h>
-#include <fstream>
+#include <intrin.h>
+#include <cstdio>
 
 #include "resource.h"
 
@@ -10,7 +11,27 @@
 #pragma comment (lib, "E:\\D3D9\\d3dx9.lib")
 #pragma comment (lib, "winmm.lib")
 
-static char $[0x10000];
+//#pragma comment(linker, "/FORCE:MULTIPLE")
+//#pragma comment(linker, "/FILEALIGN:1024")
+//#pragma comment(linker,     "/ALIGN:1024")// Merge sections
+//#pragma comment(linker, "/MERGE:.rdata=.data")
+//#pragma comment(linker, "/MERGE:.text=.data")// 252K WTF
+#pragma optimize("gsy", on)
+
+#define LODWORD(_qw)    ((DWORD)(_qw))
+#define HIDWORD(_qw)    ((DWORD)(((_qw) >> 32) & 0xffffffff))
+
+void * __cdecl operator new(unsigned int bytes)
+{
+	return HeapAlloc(GetProcessHeap(), 0, bytes);
+}
+
+void __cdecl operator delete(void *ptr)
+{
+	if (ptr) HeapFree(GetProcessHeap(), 0, ptr);
+}
+
+static char*$;
 
 static LPDIRECT3DTEXTURE9	$T[4];
 static LPD3DXSPRITE			drawing;
@@ -19,8 +40,7 @@ static D3DXVECTOR2			position;
 static LPD3DXFONT			$F[4];
 static HFONT				$Fh[16];
 static USHORT				tick = 16;
-static LPSTR				test = "ultraFx",
-							uFxC = "uFxSys";
+static LPSTR				uFxC = "uFxSys";
 
 static LPDIRECT3D9			d3d;
 static LPDIRECT3DDEVICE9	d3ddev;
@@ -32,26 +52,48 @@ static BYTE		lives;
 static HBITMAP	common_gfx[4];
 static HWND		int_hwnd[4];
 
-static char CHR[0x10000];
-static char PRG[0x10000];
-static char GFX[0x10000];
-static char SFX[0x10000];
+static char*STR, *PRG, *GFX, *SFX;
+static char END[] = "END     ÿÿÿÿÿÿÿÿ";
 
-//#define CRYPT8(str) { CRYPT8_(str "\0\0\0\0\0\0\0\0") }
-//#define CRYPT8_(str) (str)[0] + 1, (str)[1] + 2, (str)[2] + 3, (str)[3] + 4, (str)[4] + 5, (str)[5] + 6, (str)[6] + 7, (str)[7] + 8, '\0'
-
-void xor(char*in,size_t length,char*key,size_t keylen)
+int* xor(char*in,size_t length,char*key,size_t keylen)
 {
 	size_t i;
 	for (i = 0; i < length; i++)
+		//if (in[i])
 		in[i] ^= key[i % keylen];
-	//return (int*)(in + i);
+	return (int*)(in+i); // <-- imitating old programming thing to piss you off
 }
 
 UINT xorkle = 0xD;
-char*xorkey = "\x13\x07\x04\x1A\x12\x0F\x04\x00\x0A\x04\x00\x12\x18";//"the speakeasy";//"\x8B\x97\x9A\xDF\x8C\x8F\x9A\x9E\x94\x9A\x9E\x8C\x86";
+char*xorkey = "\x13\x07\x04\x1A\x12\x0F\x04\x00\x0A\x04\x00\x12\x18";//"the speakeasy";
 
-using namespace std;
+#define MAKELLONG(a,b) (long long) a << 32 | b;
+
+ULONGLONG muhLookupTable(char*mem, char*id)
+{
+	char* a = mem;
+	char* b = new char[8];
+
+	strncpy(b, id, 8);
+
+	for (int i = 0; i < 8; i++)
+		if (!b[i])
+			b[i] = ' ';
+
+	while (strncmp(a, END, 8))
+	{
+		if (!strncmp(a, b, 8))
+			break;
+		else
+			a += 16;
+	}
+
+	int addr, len;
+	CopyMemory(&len, a + 0x8, sizeof(DWORD));
+	CopyMemory(&addr, a + 0xC, sizeof(DWORD));
+
+	return MAKELLONG(_byteswap_ulong(len), _byteswap_ulong(addr));
+}
 
 static bool playSnd(LPCSTR fname)
 {
@@ -66,6 +108,12 @@ static void NewTexture(LPCSTR fname, D3DFORMAT fmt, LPDIRECT3DTEXTURE9 *tex)
 		D3DX_DEFAULT, 0, fmt, D3DPOOL_DEFAULT, D3DX_DEFAULT,
 		D3DX_DEFAULT, D3DCOLOR_XRGB(255, 0, 255), NULL, NULL, tex);
 }
+
+/*void WinMainCRTStartup()
+{
+	int Result = WinMain(GetModuleHandle(0), 0, 0, 0);
+	ExitProcess(Result);
+}*/
 
 static LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
@@ -84,31 +132,35 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hbrBackground = (HBRUSH)CreateSolidBrush(0);
 	wc.lpszClassName = uFxC;
 
 	RegisterClassEx(&wc);
-
+	
 	hWnd = CreateWindowEx(NULL,
 		uFxC,
-		"ultraFx",
-		WS_OVERLAPPED | WS_SYSMENU + SS_BITMAP,
-		GetSystemMetrics(SM_CXSCREEN) / 2 - 1024 / 2,
-		GetSystemMetrics(SM_CYSCREEN) / 2 - 768 / 2,
-		1024 + 6,
-		768 + 34,
+		0,
+		WS_OVERLAPPED | WS_SYSMENU,
+		GetSystemMetrics(SM_CXSCREEN) / 2 - 640 / 2,
+		GetSystemMetrics(SM_CYSCREEN) / 2 - 480 / 2,
+		640 + 6,
+		480 + 34,
 		NULL,
 		NULL,
 		hInstance,
 		NULL);
 
 	{
-		int_hwnd[0] = CreateWindowExA(0, "static", "Loading!!!", WS_CHILD | WS_VISIBLE, 16, 16, 64, 16, hWnd, 0, hInstance, 0);
-		//common_gfx[0] = LoadBitmap(hInstance, MAKEINTRESOURCE(COM_GFX_LD0));
-		//common_gfx[1] = LoadBitmap(hInstance, MAKEINTRESOURCE(COM_GFX_LD1));
-		//int_hwnd[1] = CreateWindowExA(0, "static", "COM_GFX_LD0", WS_CHILD | WS_VISIBLE, 16, 40, 21, 22, hWnd, 0, hInstance, common_gfx[0]);
-		//int_hwnd[2] = CreateWindowExA(0, "static", "COM_GFX_LD1", WS_CHILD | WS_VISIBLE, 16, 66, 21, 22, hWnd, 0, hInstance, 0);
-		//SendMessage(int_hwnd[1], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)common_gfx[0]);
+		$ = new char[0x10000];
+		LoadString(hInstance, COM_STR_UFXT, $ + 0x93E0, 0x20);
+		SetWindowText(hWnd, $ + 0x93E0);
+		LoadString(hInstance, COM_STR_LD0, $ + 0x9400, 0x20);
+		int_hwnd[0] = CreateWindowExA(0, "static", $ + 0x9400, WS_CHILD | WS_VISIBLE, 16, 16, 112, 16, hWnd, 0, hInstance, 0);
+		common_gfx[0] = LoadBitmap(hInstance, MAKEINTRESOURCE(COM_GFX_LD0));
+		common_gfx[1] = LoadBitmap(hInstance, MAKEINTRESOURCE(COM_GFX_LD1));
+		int_hwnd[1] = CreateWindowExA(0, "static", "COM_GFX_LD0", WS_CHILD | WS_VISIBLE | SS_BITMAP, 16, 40, 21, 22, hWnd, 0, hInstance, common_gfx[0]);
+		//int_hwnd[2] = CreateWindowExA(0, "static", "COM_GFX_LD1", WS_CHILD | WS_VISIBLE | SS_BITMAP, 16, 66, 21, 22, hWnd, 0, hInstance, 0);
+		SendMessage(int_hwnd[1], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)common_gfx[0]);
 	}
 
 	ShowWindow(hWnd, nCmdShow);
@@ -119,43 +171,72 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	//}
 
 	{
-		ifstream file;
-		file.open("STR", ios::in | ios::binary);
-		//file.seekg(0x100);
-		if (file.read($, 0x200))
+		UpdateWindow(hWnd);
+
+		// D3D INIT
 		{
-			xor ($, 0x200, xorkey, xorkle);
-			//sprintf_s($,"%d",file.gcount());
-			SetWindowText(hWnd, $+0x100);
+			d3d = Direct3DCreate9(D3D_SDK_VERSION);
+
+			D3DPRESENT_PARAMETERS d3dpp;
+
+			ZeroMemory(&d3dpp, sizeof(d3dpp));
+
+			d3dpp.Windowed = true;
+			d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+			d3dpp.hDeviceWindow = hWnd;
+			d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;
+
+			d3d->CreateDevice(D3DADAPTER_DEFAULT,
+				D3DDEVTYPE_HAL,
+				hWnd,
+				D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+				&d3dpp,
+				&d3ddev);
+
+			d3ddev->SetRenderState(D3DRS_LIGHTING, FALSE);
+
+			d3ddev->SetRenderState(D3DRS_ZENABLE, FALSE);
+
+			D3DXCreateSprite(d3ddev, &drawing);
 		}
 
-		DestroyWindow(int_hwnd[0]);
+		LoadString(hInstance, COM_STR_LD1, $ + 0x9420, 0x20);
+		SetWindowText(int_hwnd[0], $ + 0x9420);
+		UpdateWindow(hWnd);
 
-		d3d = Direct3DCreate9(D3D_SDK_VERSION);
+		// FILE LOADING
+		/**
+		{
+			FILE*file;
+			fopen_s(&file, "STR", "rb");
+			STR = new char[0x10000];
+			if (fread_s(STR, 0x200, 1, 0x200, file))
+			{
+				//xor(STR, 0x200, xorkey, xorkle);
+				SetWindowText(hWnd, STR + LODWORD(muhLookupTable(STR, "TITLE")));
+			}
+		}
+		/**/
+		{
+			HANDLE file;
+			file = CreateFile("STR", GENERIC_READ, FILE_SHARE_READ, 0,
+				OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+			STR = new char[0x10000];
+			
+			if (file != INVALID_HANDLE_VALUE)
+				if (ReadFile(file, STR, 0x200, 0, 0))
+					SetWindowText(hWnd, STR + LODWORD(muhLookupTable(STR, "TITLE")));
+		}
+		
+		SetWindowText(int_hwnd[0], $ + 0x9420);
+		SendMessage(int_hwnd[1], STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)common_gfx[1]);
+		UpdateWindow(hWnd);
 
-		D3DPRESENT_PARAMETERS d3dpp;
-
-		ZeroMemory(&d3dpp, sizeof(d3dpp));
-
-		d3dpp.Windowed = true;
-		d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-		d3dpp.hDeviceWindow = hWnd;
-		d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;
-
-		d3d->CreateDevice(D3DADAPTER_DEFAULT,
-			D3DDEVTYPE_HAL,
-			hWnd,
-			D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-			&d3dpp,
-			&d3ddev);
-
-		d3ddev->SetRenderState(D3DRS_LIGHTING, FALSE);
-
-		d3ddev->SetRenderState(D3DRS_ZENABLE, FALSE);
-
-		D3DXCreateSprite(d3ddev, &drawing);
+		Sleep(40);
 	}
-	
+
+	DestroyWindow(int_hwnd[0]);
+	DestroyWindow(int_hwnd[1]);
 
 	static MSG msg;
 
